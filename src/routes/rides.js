48a33c0cb0be21edf563808067ed844e123409de
Artem <1992bress@gmail.com>
2021-08-21 13:26:48 +0300

    id1 feat(doc): add docs

--- src/routes/rides.js
+++ src/routes/rides.js
@@ -1,16 +1,9 @@
-'use strict';
+const express = require("express");
+const router = express.Router();
 
-const express = require('express');
-const app = express();
 
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
-
-module.exports = (db) => {
-    app.get('/health', (req, res) => res.send('Healthy'));
-
-    app.post('/rides', jsonParser, (req, res) => {
-        const startLatitude = Number(req.body.start_lat);
+router.post('/rides', (req, res) => {
+    const startLatitude = Number(req.body.start_lat);
         const startLongitude = Number(req.body.start_long);
         const endLatitude = Number(req.body.end_lat);
         const endLongitude = Number(req.body.end_long);
@@ -19,102 +12,102 @@ module.exports = (db) => {
         const driverVehicle = req.body.driver_vehicle;
 
         if (startLatitude < -90 || startLatitude > 90 || startLongitude < -180 || startLongitude > 180) {
-            return res.send({
+            return res.status(400).send({
                 error_code: 'VALIDATION_ERROR',
-                message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
+                message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
             });
         }
 
         if (endLatitude < -90 || endLatitude > 90 || endLongitude < -180 || endLongitude > 180) {
-            return res.send({
+            return res.status(400).send({
                 error_code: 'VALIDATION_ERROR',
-                message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
+                message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
             });
         }
 
         if (typeof riderName !== 'string' || riderName.length < 1) {
-            return res.send({
+            return res.status(400).send({
                 error_code: 'VALIDATION_ERROR',
-                message: 'Rider name must be a non empty string'
+                message: 'Rider name must be a non empty string',
             });
         }
 
         if (typeof driverName !== 'string' || driverName.length < 1) {
-            return res.send({
+            return res.status(400).send({
                 error_code: 'VALIDATION_ERROR',
-                message: 'Rider name must be a non empty string'
+                message: 'Rider name must be a non empty string',
             });
         }
 
         if (typeof driverVehicle !== 'string' || driverVehicle.length < 1) {
-            return res.send({
+            return res.status(400).send({
                 error_code: 'VALIDATION_ERROR',
-                message: 'Rider name must be a non empty string'
+                message: 'Rider name must be a non empty string',
             });
         }
 
         var values = [req.body.start_lat, req.body.start_long, req.body.end_lat, req.body.end_long, req.body.rider_name, req.body.driver_name, req.body.driver_vehicle];
         
-        const result = db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
+        const result = req.app.db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
             if (err) {
-                return res.send({
+                return res.status(500).send({
                     error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
+                    message: 'Unknown error',
                 });
             }
 
             db.all('SELECT * FROM Rides WHERE rideID = ?', this.lastID, function (err, rows) {
                 if (err) {
-                    return res.send({
+                    return res.status(500).send({
                         error_code: 'SERVER_ERROR',
-                        message: 'Unknown error'
+                        message: 'Unknown error',
                     });
                 }
 
                 res.send(rows);
             });
         });
-    });
+})
 
-    app.get('/rides', (req, res) => {
-        db.all('SELECT * FROM Rides', function (err, rows) {
+router.get('/rides', (req, res) => {
+    req.app.db.all('SELECT * FROM Rides', function (err, rows) {
             if (err) {
-                return res.send({
+                return res.status(500).send({
                     error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
+                    message: 'Unknown error',
                 });
             }
 
             if (rows.length === 0) {
-                return res.send({
+                return res.status(404).send({
                     error_code: 'RIDES_NOT_FOUND_ERROR',
-                    message: 'Could not find any rides'
+                    message: 'Could not find any rides',
                 });
             }
 
             res.send(rows);
         });
-    });
+})
 
-    app.get('/rides/:id', (req, res) => {
-        db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, function (err, rows) {
+router.get('/rides/:id', (req, res) => {
+        req.app.db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, function (err, rows) {
             if (err) {
-                return res.send({
+                return res.status(500).send({
                     error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
+                    message: 'Unknown error',
                 });
             }
-
+            
             if (rows.length === 0) {
-                return res.send({
+                return res.status(404).send({
                     error_code: 'RIDES_NOT_FOUND_ERROR',
-                    message: 'Could not find any rides'
+                    message: 'Could not find any rides',
                 });
             }
 
             res.send(rows);
         });
-    });
+})
 
-    return app;
-};
+
+module.exports = router;
